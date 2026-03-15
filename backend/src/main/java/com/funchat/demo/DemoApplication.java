bec65b1efd12f293fe9834ee7221d4eb38e74e76
changbill <changbill@naver.com>
2026-03-15 16:52:11 +0900

    fix: 빈 주입 및 .env 환경변수 오류 수정

--- backend/src/main/java/com/funchat/demo/DemoApplication.java
+++ backend/src/main/java/com/funchat/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.funchat.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("USER_ID", dotenv.get("USER_ID"));
+		System.setProperty("USER_PW", dotenv.get("USER_PW"));
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- backend/src/main/java/com/funchat/demo/chat/service/ChatService.java
+++ backend/src/main/java/com/funchat/demo/chat/service/ChatService.java
@@ -1,14 +1,16 @@
 package com.funchat.demo.chat.service;
 
 import com.funchat.demo.chat.domain.dto.MessageResponse;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ChatService {
 
+    MessageBrokerChatService messageBrokerChatService;
 
     public List<MessageResponse> getChatMessages (Long roomId) {
-
         return null;
     }
 }
