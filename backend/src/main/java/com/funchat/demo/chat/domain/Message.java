524ce7d2c7841368c0661fef76e607101e678e16
changbill <changbill@naver.com>
2026-03-17 21:11:43 +0900

    feat: Message 도메인 완료

--- backend/src/main/java/com/funchat/demo/chat/domain/Message.java
+++ backend/src/main/java/com/funchat/demo/chat/domain/Message.java
@@ -1,9 +1,12 @@
 package com.funchat.demo.chat.domain;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
@@ -16,12 +19,17 @@ import java.time.LocalDateTime;
 @Getter
 public class Message implements Serializable {
     @Id
-    private String id;
+    private String id; // MongoDB의 _id (ObjectId)
+
+    @Indexed // 쿼리 성능을 위해 인덱스 추가
     private Long roomId;
+
     private Long senderId;
     private String senderNickname;
-    private String senderImageUrl;
-    private String message;
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private MessageType type; // TEXT, IMAGE, SYSTEM 등
 
     private LocalDateTime createdAt;
 
---
+++ backend/src/main/java/com/funchat/demo/chat/domain/MessageType.java
@@ -0,0 +1,5 @@
+package com.funchat.demo.chat.domain;
+
+public enum MessageType {
+    TEXT, IMAGE, SYSTEM
+}
