c5124439f52579c3838695ec94b78ef7baa71eb8
changbill <changbill@naver.com>
2026-03-24 17:32:47 +0900

    feat: redis 의존성 주입

--- backend/src/main/java/com/funchat/demo/global/config/RedisCacheConfig.java
+++ backend/src/main/java/com/funchat/demo/global/config/RedisCacheConfig.java
@@ -11,9 +11,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import tools.jackson.databind.ObjectMapper;
-import tools.jackson.databind.SerializationFeature;
 import tools.jackson.databind.json.JsonMapper;
 
 import java.time.Duration;
@@ -32,13 +30,9 @@ public class RedisCacheConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        // 1. JSON 변환을 위한 ObjectMapper 설정
         ObjectMapper mapper = new JsonMapper();
-
-        // 2. Serializer 생성
         GenericJacksonJsonRedisSerializer jsonSerializer = new GenericJacksonJsonRedisSerializer(mapper);
 
-        // 3. Key와 Value에 Serializer 적용
         template.setKeySerializer(new StringRedisSerializer()); // 키는 평범한 문자열로
         template.setValueSerializer(jsonSerializer);            // 값(객체)은 JSON으로
 
--- backend/src/main/java/com/funchat/demo/global/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/funchat/demo/global/filter/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.funchat.demo.global.filter;
 import com.funchat.demo.auth.service.CustomUserDetailsService;
 import com.funchat.demo.auth.service.JwtTokenProvider;
 import com.funchat.demo.auth.util.AuthUtil;
+import com.funchat.demo.chat.service.redis.RedisService;
 import com.funchat.demo.global.exception.BusinessException;
 import com.funchat.demo.global.exception.ErrorCode;
 import jakarta.servlet.FilterChain;
@@ -10,7 +11,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,7 +24,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final CustomUserDetailsService userDetailsService;
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisService redisService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -33,11 +33,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         Optional<String> optionalToken = AuthUtil.resolveToken(request.getHeader("Authorization"));
         if (optionalToken.isPresent()) {
             jwtTokenProvider.validateAccessToken(optionalToken);
+            String accessToken = optionalToken.get();
 
-            if (Boolean.TRUE.equals(redisTemplate.hasKey("blacklist:" + optionalToken)))
+            if (Boolean.TRUE.equals(redisService.isBlacklisted(accessToken)))
                 throw new BusinessException(ErrorCode.ALREADY_LOGOUT_ACCESS_TOKEN);
 
-            String accessToken = optionalToken.get();
             String email = jwtTokenProvider.getEmail(accessToken);
             UserDetails userDetails = userDetailsService.loadUserByUsername(email);
             UsernamePasswordAuthenticationToken auth =
