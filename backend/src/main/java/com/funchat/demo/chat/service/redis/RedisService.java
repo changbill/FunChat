8c3f05efa2b6ff083e15dbc4e76cb6adebea566d
changbill <changbill@naver.com>
2026-04-08 20:07:50 +0900

    fix: redis에 현재 사용자 참여하고 있는 채팅방 기록 기능 삭제

--- backend/src/main/java/com/funchat/demo/chat/service/redis/RedisService.java
+++ backend/src/main/java/com/funchat/demo/chat/service/redis/RedisService.java
@@ -13,7 +13,6 @@ public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
 
     private String REFRESH_TOKEN_PREFIX = "RT:";
-    private String USER_CURRENT_ROOM_PREFIX = "user:pos:";
     private String BLACKLIST_PREFIX = "blacklist:";
 
     public String getRefreshToken(String email) {
@@ -30,22 +29,6 @@ public class RedisService {
 
     public void deleteRefreshToken(String email) {
         redisTemplate.delete(REFRESH_TOKEN_PREFIX + email);
-    }
-
-    public Long getUserCurrentRoomId(Long userId) {
-        return (Long) redisTemplate.opsForValue().get(USER_CURRENT_ROOM_PREFIX + userId);
-    }
-
-    public void saveUserCurrentRoomId(Long userId, Long roomId) {
-        redisTemplate.opsForValue().set(
-                USER_CURRENT_ROOM_PREFIX + userId,
-                roomId,
-                Duration.ofHours(2)
-        );
-    }
-
-    public void deleteUserCurrentRoomId(Long userId) {
-        redisTemplate.delete(USER_CURRENT_ROOM_PREFIX+ userId);
     }
 
     public void saveBlacklist(String accessToken, String logout, Duration duration) {
