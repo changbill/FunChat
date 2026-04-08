5604cdef7b749b781b53110b075064a1521e34f1
changbill <changbill@naver.com>
2026-04-08 20:10:15 +0900

    fix: objectMapper 직렬화, 역직렬화 관련 오류 해결

--- backend/src/main/java/com/funchat/demo/room/domain/dto/RoomRequest.java
+++ backend/src/main/java/com/funchat/demo/room/domain/dto/RoomRequest.java
@@ -9,7 +9,6 @@ public record RoomRequest(
         @NotBlank(message = "방 제목은 필수입니다.")
         String title,
         @Min(value = 2, message = "최소 인원은 2명 이상이어야 합니다.")
-        Integer maxMembers,
-        Long managerId
+        Integer maxMembers
 ) {
 }
